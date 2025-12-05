71862ca3730fcb09909fe2a2372c0179a1a60c41
FernandoPerez233 <al45722@un.edu.mx>
2025-12-05 09:53:47 -0600

    cambio 1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function img{
+var imagen = document.querySelectorAll("imagenes");
 
-}
+imagen = !imagen;
+
+imagen.addEventListener('click', function() {
+    if(imagen.width === "350px"){
+        imagen.style.width = "500px";
+    } 
+    else{
+        imagen.style.width = "350px";
+    }
+});
+
+
